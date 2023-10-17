a7ba043d36891dadf644458086d99f6784b92cb8
bruno <chichun920718@gmail.com>
2023-10-17 20:35:59 +0800

    renew

---
+++ javaSourcecode/mainclass.java
@@ -0,0 +1,9 @@
+//import java.sql.*;
+
+public class mainclass{                              
+	public static void main (String[] args){ 
+		totalinfo info= new totalinfo();
+		initsol firstsol=new initsol();
+		firstsol.createfun(info);
+	}
+}
