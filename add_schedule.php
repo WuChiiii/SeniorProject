0776d8dfaebe9f652ad718692f82ee169e3b070d
bruno <chichun920718@gmail.com>
2023-12-08 13:14:35 +0800

    environment_set

--- add_schedule.php
+++ add_schedule.php
@@ -3,11 +3,11 @@
 if (isset($_POST['add_schedule_submit'])) {
     $path_back =getcwd();
     $path_to_source =getcwd() ."\javaSourcecode";
-    $path_to_jar="";//enter your absolute path of jar.exe;
+    $path_to_jar=getcwd() ."/environment/jar.exe";
     //example of my pathtojar:$path_to_jar="C:\Program Files\Java\jdk-20\bin\jar.exe";
-    $path_to_javac="";//enter your absolute path of javac.exe 
+    $path_to_javac=getcwd() ."/environment/javac.exe";
     //example of my pathtojavac:$path_to_javac="C:\Program Files\Java\jdk-20\bin\javac.exe";
-    $java_Exe ="";//enter your absolute path of java.exe 
+    $java_Exe =getcwd() ."/environment/java.exe"; 
     //example of my javapath:$java_Exe = "C:\Program Files\Java\jdk-20\bin\java.exe";
     shell_exec( "\"$path_to_javac\"-encoding utf-8 $path_to_source\*.java" );
     shell_exec("cd $path_to_source && \"$path_to_jar\" -cvfm mainclass.jar MANIFEST.MF *.class");
